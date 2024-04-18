79dff3fc67c98baa8984f7c39ab9f9436ec53643
Ku-Zag <abdullamcidov2003@gmail.com>
2024-04-18 20:02:52 +0000

    updated 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,24 +7,23 @@
  */
 size_t _binary_tree_height(const binary_tree_t *tree)
 {
-        int r_height, l_height;
+	int r_height, l_height;
 
-        if (!tree)
-                return (0);
-        r_height = _binary_tree_height(tree->right);
-        l_height = _binary_tree_height(tree->left);
-        return (1 + ((r_height >= l_height) ? r_height: l_height));
+	if (!tree)
+		return (0);
+	r_height = _binary_tree_height(tree->right);
+	l_height = _binary_tree_height(tree->left);
+	return (1 + ((r_height >= l_height) ? r_height: l_height));
 }
-
 /**
- * binary_tree_balance - Measures balance of binary tree.
+ * binary_tree_balance - measures balance of binary tree.
  * @tree: pointer to root
  * Description: ptr to rt
  * Return: If tree is NULL, return 0, else return balance factor.
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-        if (!tree)
-                return (0);
-        return (_binary_tree_height(tree->left) - _binary_tree_height(tree->right));
+	if (!tree)
+		return (0);
+	return (_binary_tree_height(tree->left) - _binary_tree_height(tree->right));
 }
